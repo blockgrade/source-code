c91bf9782dbce43736a1d666fde8d5e5a43a30af
jsuisjuan <juanc.s.delima@gmail.com>
2024-08-21 10:19:08 -0300

    adicionando arquivos de contrato

---
+++ contracts/NoteRecord.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract NoteRecord {
+    //Armazenar e gerenciar os documentos de notas de provas na blockchain.
+
+    //Registrar Nota: Função que permite ao professor, identificado pela sua identidade digital, registrar um documento de notas. Esse documento pode ser um JSON contendo informações como ID do aluno, nota, data da prova, etc.
+    
+    //Validar Nota: Função para que o professor valide o documento registrado, confirmando sua autenticidade.
+    
+    //Consultar Nota: Função para que o aluno consulte suas notas associadas, retornando os dados relevantes (que podem incluir apenas metadados on-chain e detalhes mais amplos off-chain).
+}
---
+++ contracts/TransactionHistory.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract TransactionHistory {
+    //Rastrear o histórico das interações com os documentos de notas, como registros, validações, e consultas.
+
+    //Registrar Transação: Função que registra cada interação relevante com os documentos de notas, armazenando informações como o identificador da transação, o usuário envolvido (professor ou aluno), e o tipo de ação realizada.
+    
+    //Consultar Histórico: Função para que alunos ou professores visualizem o histórico das transações relacionadas a um documento específico.
+}
