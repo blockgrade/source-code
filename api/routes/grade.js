a2e4c7984956d615fdd2906f10e5bb3fa31f7744
Gilmar Santos <gneto1999@hotmail.com>
2024-08-24 22:31:45 -0300

    correção do .enc e adição do hashamento do arquivo recebido do cliente

--- api/routes/grade.js
+++ api/routes/grade.js
@@ -1,19 +1,21 @@
 import express from "express"
 import { ethers } from "ethers"
-import 'dotenv/config.js'
+import dotenv from 'dotenv'
 import abi from "../../artifacts/contracts/GradeRecord.sol/GradeRecord.json" assert { type: "json" }
 import path from "path"
 import multer from "multer"
+import calculateHash from '../utils/fileHash'
 
-// dotenv.config({ path: '../../.env' })
+dotenv.config()
 
 const API_URL = process.env.API_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const contractAddress = process.env.CONTRACT_ADDRESS
 
+console.log('DOT ENV PORRAAAA')
 console.log(API_URL, PRIVATE_KEY, contractAddress)
 
-const provider = new ethers.providers.JsonRpcProvider(API_URL)
+const provider = new ethers.JsonRpcProvider(API_URL)
 const signer = new ethers.Wallet(PRIVATE_KEY, provider)
 const contractInstance = new ethers.Contract(contractAddress, abi, signer)
 
@@ -66,8 +68,14 @@ router.get("/grade/", async (req, res) => {
     }
 })
 
-router.post("/grade/", upload.single('file'), (req, res) => {
-    res.status(200).send('Arquivo PDF enviado com sucesso!')
+router.post("/grade/", upload.single('file'), async (req, res) => {
+    try {
+        const filePath = path.join('uploads', req.file.filename)
+        const fileHash = await calculateHash(filePath)
+        res.json({ message: 'Arquivo PDF enviado com sucesso!', hash: fileHash })
+    } catch (err) {
+        res.status(500).json({ error: 'Erro ao calcular o hash do arquivo.' })
+    }
 })
 
 export default router
